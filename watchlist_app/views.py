98578a5ecabb08a92406628657a4ccb1a9509b4e
Mark <muchirimark2@gmail.com>
2022-08-01 11:44:25 +0300

    displayed the data in the database to the movie_list.html

--- watchlist_app/views.py
+++ watchlist_app/views.py
@@ -1,8 +1,10 @@
 from importlib.resources import path
+from multiprocessing import context
 from django.shortcuts import render
 from .models import Movie
 
 # Create your views here.
 def movie_list(request):
     movies=Movie.objects.all().order_by('id')
-    return render(request, 'movies/movie_list.html')
+    context={'movies':movies,}
+    return render(request, 'movies/movie_list.html', context)

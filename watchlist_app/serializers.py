db2b4f6793b2e966cef4dc2e32da3d9c5a48c56d
Mark <muchirimark2@gmail.com>
2022-08-03 12:26:20 +0300

    worked on the GET, POST, PUT and DELETE APIs

--- watchlist_app/serializers.py
+++ watchlist_app/serializers.py
@@ -1,7 +1,24 @@
 from rest_framework import serializers
+from watchlist_app .models import Movie
 
 class MovieSerializer(serializers.Serializer):
     id = serializers.IntegerField(read_only=True)
-    name  = serializers.CharField(max_length=50)
-    description = serializers.CharField(max_length=250)
+    movie_name  = serializers.CharField(max_length=50)
+    movie_description = serializers.CharField(max_length=250)
     is_active = serializers.BooleanField(default=True)
+    
+    def create(self, validated_data):
+        """
+        Create and return a new `Movie` instance, given the validated data.
+        """
+        return Movie.objects.create(**validated_data)
+
+    def update(self, instance, validated_data):
+        """
+        Update and return an existing `Snippet` instance, given the validated data.
+        """
+        instance.movie_name = validated_data.get('movie_name', instance.movie_name)
+        instance.movie_description = validated_data.get('movie_description', instance.movie_description)
+        instance.is_active = validated_data.get('is_active', instance.is_active)
+        instance.save()
+        return instance
--- watchlist_app/views.py
+++ watchlist_app/views.py
@@ -1,22 +1,57 @@
 from importlib.resources import path
 from multiprocessing import context
-from urllib import response
+from urllib import request, response
 from django.shortcuts import render
 
 from watchlist_app import serializers
 from .models import Movie
+from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from watchlist_app.serializers import MovieSerializer
 
 # Create your views here.
+@api_view(['GET', 'POST'])
 def movie_list(request):
-     if request.method == 'GET':
+    if request.method == 'GET':
         movies=Movie.objects.all().order_by('id')
         serializer=MovieSerializer(movies, many=True)
         context={'movies':movies,}
-        return render(request, 'movies/movie_list.html', context)
-    
+        return Response(serializer.data)
+        # return render(request, 'movies/movie_list.html', context)
+    if request.method == 'POST':
+        serializer = MovieSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        else:
+            return Response(serializer.errors)
+
+
+@api_view(['GET', 'POST'])
 def movie_detail(request, pk):
-    movie = Movie.objects.get(pk=pk)
-    serializer = MovieSerializer(movie)
-    return Response(serializer.data)
+    if request.method == 'GET':
+        movies = Movie.objects.all()
+        serializer = MovieSerializer(movies, many=True)
+        return Response(serializer.data)
+    if request.method == 'POST':
+        serializer = MovieSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        else:
+            return Response(serializer.errors)
+
+@api_view(['GET', 'PUT','DELETE'])
+def movie_detail(request,pk):
+    if request.method == 'GET':
+        movie=Movie.objects.get(pk=pk)
+        serializer=MovieSerializer(movie)
+        return Response (serializer.data)
+    if request.method =='PUT':
+        movie=Movie.objects.get(pk=pk)
+        serializer=MovieSerializer(movie,request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        else:
+            return Response(serializer.errors)
